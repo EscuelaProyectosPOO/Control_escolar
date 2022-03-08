db9f50efc4f307b580e86f04900113781318a3ad
Estefania <tetillamas@gmail.com>
2022-03-07 19:28:03 -0600

    Comienzo del proyecto

---
+++ src/Archivos/ArchivosMetodosGenerales.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Archivos;
+
+/**
+ *
+ * @author tetil
+ */
+public class ArchivosMetodosGenerales {
+    
+    private String nombre_archivo;
+    private String ruta_archivo;
+    
+    public ArchivosMetodosGenerales(){
+        
+        
+        ruta_archivo = "src\\Datosarchivos\\";
+    
+    }
+    
+    
+    
+}
